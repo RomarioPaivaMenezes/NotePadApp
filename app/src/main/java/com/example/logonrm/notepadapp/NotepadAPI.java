aeb9d896ddfef7fa5edf5cd778139d24f123cbaa
logonrm <romarioinf@gmail.com>
2017-11-27 23:09:13 -0200

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/notepadapp/NotepadAPI.java
@@ -0,0 +1,18 @@
+package com.example.logonrm.notepadapp;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface NotepadAPI {
+
+    @GET(value = "/nota/titulo/{titulo}")
+    Call<Nota> buscar(@Path(value = "titulo") String titulo);
+
+    @POST(value = "/nota/")
+    Call<Void> salvar(@Body Nota nota);
+
+
+}
